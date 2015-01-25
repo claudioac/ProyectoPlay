f2fdafe2a44729740960f402b7c44aee6495abe1
claudioac <claudioacunacuna@gmail.com>
2015-01-25 00:06:38 -0300

    ficha de salud

---
+++ app/models/ClasesDTO/FichaDeSaludDTO.java
@@ -0,0 +1,18 @@
+package models.ClasesDTO;
+
+import java.util.Date;
+
+/**
+ * Created by claudio on 24-01-15.
+ */
+public class FichaDeSaludDTO {
+    public String altKeyCliente;
+    public Float estatura;
+    public Float peso;
+    public Float imc;
+    public Float img;
+    public String problemasDeSalud;
+    public Long numeroDeFicha;
+    public Date fechaDeControl;
+    public String estadoDeCliente;
+}
